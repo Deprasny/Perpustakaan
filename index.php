31f85be24f03b9d55e8cce505852ab583d2e2475
randyramadhan1973 <okerandy28@outlook.com>
2019-12-14 15:32:24 +0700

    MENDESIGN TABLE V.1

--- index.php
+++ index.php
@@ -2,72 +2,74 @@
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Aplikasi Perpustakaan</title>
-    <!-- Bootsrap -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<title>Aplikasi Perpustakaan</title>
+	<!-- Bootsrap -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 </head>
 
 <body>
-    <nav class="navbar navbar-expand-lg navbar-light bg-blue">
-        <a class="navbar-brand" href="#">PerpusKu.com</a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
+	<nav class="navbar navbar-expand-lg navbar-light bg-blue">
+		<a class="navbar-brand" href="#">PerpusKu.com</a>
+		<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+			<span class="navbar-toggler-icon"></span>
+		</button>
 
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav mr-auto">
-                <li class="nav-item active">
-                    <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Daftar Buku</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">genre Buku</a>
-                </li>
-            </ul>
-            <form class="form-inline my-2 my-lg-0">
-                <input class="form-control mr-sm-2" type="search" placeholder="Cari" aria-label="Search">
-                <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Go!</button>
-            </form>
-        </div>
-    </nav>
-    <h2>CRUD DATA MAHASISWA - WWW.MALASNGODING.COM</h2>
-	<br/>
-	<a href="tambah.php">+ TAMBAH MAHASISWA</a>
-	<br/>
-	<br/>
-	<table border="1">
-		<tr>
-			<th>NO</th>
-			<th>Nama</th>
-			<th>Pengarang</th>
-			<th>Penerbit</th>
-			<th>OPSI</th>
-		</tr>
-		<?php 
-		include 'koneksi.php';
-		$no = 1;
-		$data = mysqli_query($koneksi,"select * from buku");
-		while($d = mysqli_fetch_array($data)){
-			?>
-			<tr>
-				<td><?php echo $no++; ?></td>
-				<td><?php echo $d['nama']; ?></td>
-				<td><?php echo $d['pengarang']; ?></td>
-				<td><?php echo $d['penerbit']; ?></td>
-				<td>
-					<a>EDIT</a>
-					<a>HAPUS</a>
-				</td>
-			</tr>
-			<?php 
-		}
-		?>
-	</table>
+		<div class="collapse navbar-collapse" id="navbarSupportedContent">
+			<ul class="navbar-nav mr-auto">
+				<li class="nav-item active">
+					<a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+				</li>
+				<li class="nav-item">
+					<a class="nav-link" href="#">Daftar Buku</a>
+				</li>
+				<li class="nav-item">
+					<a class="nav-link" href="#">genre Buku</a>
+				</li>
+			</ul>
+			<form class="form-inline my-2 my-lg-0">
+				<input class="form-control mr-sm-2" type="search" placeholder="Cari" aria-label="Search">
+				<button class="btn btn-outline-success my-2 my-sm-0" type="submit">Go!</button>
+			</form>
+		</div>
+	</nav>
+	<div>
+		<h2 class="text-center">CRUD BUKU PERPUSTAKAAN</h2>
+		<br />
+		<br />
+		<div class="table-responsive">
+			<table class="table table-striped pr-2 pl-2">
+				<tr>
+					<th>NO</th>
+					<th>Nama</th>
+					<th>Pengarang</th>
+					<th>Penerbit</th>
+					<th>OPSI</th>
+				</tr>
+				<?php
+				include 'koneksi.php';
+				$no = 1;
+				$data = mysqli_query($koneksi, "select * from buku");
+				while ($d = mysqli_fetch_array($data)) : ?>
+					?>
+					<tr>
+						<td><?php echo $no++; ?></td>
+						<td><?php echo $d['nama']; ?></td>
+						<td><?php echo $d['pengarang']; ?></td>
+						<td><?php echo $d['penerbit']; ?></td>
+						<td>
+							<a>EDIT</a>
+							<a>HAPUS</a>
+						</td>
+					</tr>
+				<? endwhile; ?>
+				<a href="tambah.php">TAMBAH MAHASISWA</a>
+			</table>
+		</div>
+	</div>
+
 </body>
 
 </html>
---
+++ perpustakaan.sql
@@ -0,0 +1,68 @@
+-- phpMyAdmin SQL Dump
+-- version 4.9.0.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Waktu pembuatan: 14 Des 2019 pada 08.51
+-- Versi server: 10.4.6-MariaDB
+-- Versi PHP: 7.3.9
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `perpustakaan`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `buku`
+--
+
+CREATE TABLE `buku` (
+  `id_buku` int(10) NOT NULL,
+  `nama` varchar(20) NOT NULL,
+  `pengarang` varchar(20) NOT NULL,
+  `penerbit` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data untuk tabel `buku`
+--
+
+INSERT INTO `buku` (`id_buku`, `nama`, `pengarang`, `penerbit`) VALUES
+(1, 'MTK', 'ADI', 'PT.ALIM RUGI');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indeks untuk tabel `buku`
+--
+ALTER TABLE `buku`
+  ADD PRIMARY KEY (`id_buku`);
+
+--
+-- AUTO_INCREMENT untuk tabel yang dibuang
+--
+
+--
+-- AUTO_INCREMENT untuk tabel `buku`
+--
+ALTER TABLE `buku`
+  MODIFY `id_buku` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
